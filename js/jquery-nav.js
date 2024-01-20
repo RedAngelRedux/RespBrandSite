20f80fb99a120c15ffcba974e70e1ce3c6707a30
RedAngelRedux <sammy@sammynava.com>
2024-01-19 22:08:03 -0600

    jQuery Burger Template
    Create a barebones template for a jQuery burger menu, named jquery-nav.*

---
+++ js/jquery-nav.js
@@ -0,0 +1,24 @@
+// $(document).ready(function() {
+//     $('.burgerMenu').on('click',function() {
+//         $('.mobile-nav').toggle();
+//     })
+// });
+
+// $(document).ready(function() {
+//     $('.burgerMenu').on('click',function() {
+//         $('.mobile-nav').toggle('slow');
+//     })
+// });
+
+// $(document).ready(function() {
+//     $('.burgerMenu').on('click',function() {
+//         $('.mobile-nav').toggle(2000);
+//     })
+// });
+
+$(document).ready(function() {
+    $('.burgerMenu').on('click',function() {
+        $('.mobile-nav').fadeToggle();
+    })
+});
+
